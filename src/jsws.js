2d697ba537d0111ef48d657879216fc8e8b8cecd
Curt J. Sampson <cjs@cynic.net>
2015-12-03 17:03:40 +0900

    src/jsws.js: replace mock webserver with a real node.js webserver.
    This currently returns a static string, rather than reading things
    from the filesystem. The test framework also, annoyingly, prints
    "Terminated" after it shuts down the web server. This will be fixed
    when the web server deamonizes properly, though it might be worth
    looking at a little hack to the test framework to get rid of this
    in the meantime.

---
+++ src/jsws.js
@@ -0,0 +1,14 @@
+var
+    fs = require('fs'),
+    http = require('http'),
+    listen_port = process.argv[2]
+
+if (!(listen_port > 0)) {
+    console.log("Bad port argument: '" + listen_port + "'.")
+    process.exit(1);
+}
+
+http.createServer(function (req, res) {
+    res.writeHead(200)
+    res.end("Hello.\n")
+}).listen(listen_port);
